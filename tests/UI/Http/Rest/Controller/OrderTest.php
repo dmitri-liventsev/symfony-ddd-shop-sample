113e1dcf98773fcbc8c1635483d8729604e2f014
Dmitri <local-server@tacticrealtime.com>
2018-09-11 10:54:33 +0300

    test sketch

--- tests/UI/Http/Rest/Controller/OrderTest.php
+++ tests/UI/Http/Rest/Controller/OrderTest.php
@@ -12,11 +12,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class OrderTest extends WebTestCase
 {
+	public function testUserCanPurchaseProduct() {
+		//Create user
+		//Create product
+		//Purchase products via API
+		//Check order
+
+		$this->assertEquals(1,1);
+	}
+
     public function testCanBeGetListOfOrders() {
     	//Create user
 	    //Create product
 	    //Purchase products via API
 	    //Check orders number and orders !?!? Need to drop DB before using
+
+        $this->assertEquals(1,1);
+    }
+
+    public function testNumberProductsOnStockShouldBeReducedAfterPurchasing() {
+    	//Create user
+	    //Create product witch amount
+	    //Purchase products via API
+	    //Check product should contain reduced number of products
 
         $this->assertEquals(1,1);
     }
